96c84e768aed21608f83fffe0ca7593654c5063b
hayashi-yaken <mypodo@icloud.com>
2024-10-26 16:14:28 +0900

    タブの作成

---
+++ frontend/src/navigator/tab/Tab.typeDefs.ts
@@ -0,0 +1,16 @@
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+
+export type TabBarStatus = {
+  focused: boolean;
+  color: string;
+  size: number;
+};
+
+export type TabParamList = {
+  MapTab: undefined;
+  BonfireTab: undefined;
+  BookTab: undefined;
+  // add more tab params...
+};
+
+export type TabProps = BottomTabScreenProps<TabParamList, keyof TabParamList>;
